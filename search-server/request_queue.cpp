b8cbe874bc769f4ba42af5f4698deae69112f695
Aleksandr <clif05@yandex.ru>
2022-06-20 10:17:53 +0300

    Fixed Bulat's A. comments
     Added empty requests counter

--- search-server/request_queue.cpp
+++ search-server/request_queue.cpp
@@ -15,17 +15,22 @@ vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query) {
 }
 
 int RequestQueue::GetNoResultRequests() const {
-    return static_cast<int>(std::count_if(requests_.begin(), requests_.end(), [](QueryResult i) { return i.requests == 0; }));
+    //return static_cast<int>(std::count_if(requests_.begin(), requests_.end(), [](QueryResult i) { return i.requests == 0; }));
+    return empty_request;
 }
 
 void RequestQueue::AddRequest(int request_size) {
     ++counter_min_;
     if (counter_min_ > min_in_day_)
     {
+        auto& request = requests_.front();
+        if (request.requests == 0) {
+            --empty_request;
+        }
         requests_.pop_front();
-    }
+    }    
     if (request_size == 0) {
-
+        ++empty_request;
         RequestQueue::requests_.push_back({ {static_cast<int>(counter_min_)}, { 0 } });
     }
     else
